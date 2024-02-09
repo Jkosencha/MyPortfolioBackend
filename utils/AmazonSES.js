ef58bf0434f9c8b21e79139977ade88d3643ca1e
Jennifer Kosencha <100467509+Jkosencha@users.noreply.github.com>
2024-02-09 18:53:57 +0300

    region

--- utils/AmazonSES.js
+++ utils/AmazonSES.js
@@ -4,7 +4,7 @@ const ejs = require('ejs')
 
 
 const sesClient = new SESClient({
-    region:"",
+    region:"us-east-1",
     credentials:{
         accessKeyId:process.env.AMAZON_ACCESS_KEY,
         secretAccessKey:process.env.AMAZON_SECRET_KEY,
